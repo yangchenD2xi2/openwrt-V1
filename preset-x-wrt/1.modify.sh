b668ccd5552abdb10911208eea56e9f502f393e2
yangchenD2xi <yangchend2xi@163.com>
2024-11-29 22:37:15 +0800

    Update 1.modify.sh

--- preset-x-wrt/1.modify.sh
+++ preset-x-wrt/1.modify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # modify login IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # copy uci-defaults script(s)
 mkdir -p files/etc/uci-defaults
