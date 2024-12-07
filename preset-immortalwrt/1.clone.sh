a5eadd4a932ec1e804822e3a855fbc09c9e053e2
yangchenD2xi-google <yangchend2xi@gmail.com>
2024-12-07 20:21:13 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -29,4 +29,4 @@ git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
 git clone --depth 1 https://github.com/kenzok8/small-package.git
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+git clone --depth 1 https://github.com/vernesong/OpenClash.git
