53e9c29090204e80ce19abc79d4024f53c6d0887
yangchenD2xi2 <yangchend2xi2@gmail.com>
2024-12-10 15:36:24 +0800

    Update 1.clone.sh

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -28,9 +28,19 @@ git clone --depth 1 https://github.com/yichya/openwrt-xray.git
 git clone --depth 1 https://github.com/XTLS/Xray-core.git
 git clone --depth 1 https://github.com/MilesPoupart/luci-app-vssr.git
 git clone --depth 1 https://github.com/haiibo/openwrt-packages.git && mv openwrt-packages/luci-app-poweroff./&& rm -rf openwrt-packages
+git clone --depth 1 https://github.com/haiibo/openwrt-packages.git && mv openwrt-packages/luci-app-passwall./&& rm -rf openwrt-packages
+git clone --depth 1 https://github.com/haiibo/openwrt-packages.git && mv openwrt-packages/luci-app-passwall2./&& rm -rf openwrt-packages
+git clone --depth 1 https://github.com/haiibo/openwrt-packages.git && mv openwrt-packages/luci-app-netdata./&& rm -rf openwrt-packages
 git clone --depth 1 https://github.com/kongfl888/luci-app-timedreboot.git
 git clone --depth 1 https://github.com/chenmozhijin/turboacc.git
-git clone --depth 1 https://github.com/kenzok8/small-package.git
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-adguardhome./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-ddns./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-firewall./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-nlbwmon./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-opkg./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-ramfree./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-smartdns./&& rm -rf small-package
+git clone --depth 1 https://github.com/kenzok8/small-package.git && mv small-package/luci-app-upnp./&& rm -rf small-package
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/vernesong/OpenClash.git
